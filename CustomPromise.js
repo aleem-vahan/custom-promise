64429526a1c741dd9a981aeee6e7a6bfcf8c1b85
Aleem Uddin <aleemuddin966@gmail.com>
2022-09-30 18:23:35 +0530

    made some modifications

--- CustomPromise.js
+++ CustomPromise.js
@@ -1,57 +1,77 @@
 const STATE = {
     PENDING: 'pending',
     SUCCESS: 'success',
-    FAILED : 'failed'
+    FAILED: 'failed'
 }
-class CustomPromise{
+
+/**
+ * this._state -> to maintain state
+ * this._thenObject -> it will store .then func params
+ * this._catchFunc -> it will store catch function 
+ */
+class CustomPromise {
     constructor(executingFunction) {
         this._state = STATE.PENDING;
-        this._chainList = [];
-        this.resolve = this.resolve.bind(this)  
+        this.resolve = this.resolve.bind(this)
         this.reject = this.reject.bind(this)
-        try {
-          executingFunction(this.resolve, this.reject);
-        } catch (err) {
-          this.reject(err);
-        }
+        // making sure it's async using setTimeout
+        setTimeout(() => {
+            try {
+                executingFunction(this.resolve, this.reject);
+            } catch (err) {
+                this.reject(err);
+            }
+        },0)
     }
-    
-    resolve(result){
+
+    resolve(result) {
         if (this._state !== STATE.PENDING) {
-          return;
+            return;
         }
         this._state = STATE.SUCCESS;
         this._result = result;
-        for (const { onSuccess } of this._chainList) {
-          onSuccess(res);
+        if(this._thenObject && this._thenObject.onSuccess){
+            this._thenObject.onSuccess(this._result)
         }
     }
-    reject(error){
+    reject(error) {
         if (this._state !== STATE.PENDING) {
-          return;
+            return;
         }
         this._state = STATE.FAILED;
         this._result = error;
-        for (const { onFailed } of this._chainList) {
-          onFailed(error);
+        if(this._thenObject && this._thenObject.onFailed){
+            this._thenObject.onFailed(this._result)
+        }else if(this._catchFunc){
+            this._catchFunc(this._result)
         }
-    }
-        
-    then(onSuccess, onFailed) {
-        if (this._state === STATE.SUCCESS) {
-            if(onSuccess)
-                this._result = onSuccess(this._result);
-        } else if (this._state === STATE.FAILED) {
-            if(onFailed)
-                this._result = onFailed(this._result);
-        } else {
-          this._chainList.push({ onSuccess, onFailed });
-        }
-        return this
     }
 
-    catch(onFailed){
-        this.then(null, onFailed)
+    then(onSuccess, onFailed) {
+        return new CustomPromise((resolve, reject) => {
+            try {
+                if (this._state === STATE.SUCCESS) {
+                    if (onSuccess)
+                        resolve(onSuccess(this._result))
+                } else if (this._state === STATE.FAILED) {
+                    if (onFailed)
+                        resolve(onFailed(this._result))
+                    else 
+                        // if onFailed func is missing,
+                        // rejecting promise, so that it will call end chain 'catch' func
+                        reject(this._result)
+                } else {
+                    this._thenObject = { onSuccess, onFailed }
+                }
+            } catch (error) {
+                reject(error)
+            }
+
+        })
+    }
+
+    catch(catchFunc) {
+        this._catchFunc = catchFunc
     }
 
 }
--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 const CustomPromise = require('./CustomPromise')
 
 new CustomPromise((resolve, reject) => {
-    console.log("inside promise 1")
-    console.log("resolving promise 1")
-    resolve("result promise 1")
-}).then((result) => {
-    console.log("got promise 1 res1: ", result)
-    return "result 2 promise 1"
-}).then((result) => {
-    console.log("got promise 1 res2: ", result)
-}).catch((err) => {
-    console.log(err)
+    resolve("dummy-response")
+}).then((res) => {
+    console.log("then 1 got res: ", res)
+    throw new Error("error-then1")
+}).then(
+    (res) => {
+        console.log("then 2 got res: ", res)
+    }, 
+    (error) => {
+        console.log("then 2 got error:", error.message)
+        return "dummy-response-2"
+    }
+).then((res) => {
+    console.log("then 3 got res: ", res)
+    throw new Error("error-then3")
 })
-
-new CustomPromise((resolve, reject) => {
-    console.log("inside promise 2")
-    console.log("rejecting promise 2")
-    reject("rejected promise 2")
-}).then((result) => {
-    console.log(result)
-}).catch((err) => {
-    console.log("error in promise2: ", err)
+.then((res) => {
+    console.log("then 4 got res: ", res)
+})
+.catch((err) => {
+    console.log("root catch: ", err.message)
 })
